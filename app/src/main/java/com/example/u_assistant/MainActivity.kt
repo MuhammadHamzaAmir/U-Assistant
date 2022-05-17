9628b752783ee798e9736c1228918274a31ca56a
MuhammadHamzaAmir <43249821+MuhammadHamzaAmir@users.noreply.github.com>
2022-05-18 03:56:14 +0500

    Google Speech Libraries Added Audio file is put into assets connected to gts. audio file transcription works.

--- app/src/main/java/com/example/u_assistant/MainActivity.kt
+++ app/src/main/java/com/example/u_assistant/MainActivity.kt
@@ -1,11 +1,79 @@
 package com.example.u_assistant
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.api.gax.core.CredentialsProvider
+import com.google.auth.oauth2.GoogleCredentials
+import com.google.cloud.speech.v1.*
+import com.google.protobuf.ByteString
+import java.io.File
+import java.io.InputStream
+
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var speechClient: SpeechClient
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val inputStream: InputStream = assets.open("credentials.json")
+        val size: Int = inputStream.available()
+        Toast.makeText(this,size.toString(),Toast.LENGTH_LONG).show()
+
+        Log.d("before sp","LET see")
+//       //val credentials:CredentialsProvider = CredentialsProvider().credentials
+        val button:Button= findViewById(R.id.button)
+
+//        button.setOnClickListener {
+//            // Code here executes on main thread after user presses button
+//            cl(inputStream)
+//        }
+        speechClient = com.google.cloud.speech.v1.SpeechClient.create(
+            SpeechSettings.newBuilder().setCredentialsProvider(CredentialsProvider { GoogleCredentials.fromStream(inputStream) }).build()
+        )
+
+        Log.d("AFTER sp","LET see")
+
+        //Toast.makeText(this,speechClient.isShutdown.toString(),Toast.LENGTH_LONG).show()
+
+        // The path to the audio file to transcribe
+        // The path to the audio file to transcribe
+
+
+
+
+        val audioFile: InputStream = assets.open("out.wav")
+        //val file:File = File(gcsUri)
+
+        val byteString:ByteString = ByteString.copyFrom(audioFile.readBytes())
+
+        // Builds the sync recognize request
+
+        // Builds the sync recognize request
+        val config: RecognitionConfig = RecognitionConfig.newBuilder()
+            .setEncoding(RecognitionConfig.AudioEncoding.LINEAR16)
+            .setSampleRateHertz(16000)
+            .setLanguageCode("ur-PK")
+            .build()
+        val audio: RecognitionAudio = RecognitionAudio.newBuilder().setContent(byteString).build()
+
+        // Performs speech recognition on the audio file
+
+        // Performs speech recognition on the audio file
+        val response: RecognizeResponse = speechClient.recognize(config, audio)
+        val results: List<SpeechRecognitionResult> = response.resultsList
+
+        Log.d("ok",results.toString())
+
     }
 }
+
+// fun cl(inputStream:InputStream){
+//     com.google.cloud.speech.v1.SpeechClient.create(
+//         SpeechSettings.newBuilder().setCredentialsProvider(CredentialsProvider { GoogleCredentials.fromStream(inputStream) }).build()
+//     )
+// }
